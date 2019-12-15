443e701181e504fe702c609cbcad4cec30686fb9
nataliapetito <nataliarpetito@gmail.com>
2019-12-15 11:28:11 -0300

    cambios de archivos y modificaciones

--- script.js
+++ script.js
@@ -8,26 +8,59 @@ var puerto1 = process.env.PORT || 3000
 
 app.use(express.static('./recursos-estaticos'))
 
-app.get("/busqueda", async function(consulta, respuesta) {
-    await db.collection("tiposReciclados")
-    findOne( {categoria: c.params.buscar }) /*NO ENTIENDO CÓMO BUSCAR*/
-})
+app.get('/index', function(consulta, respuesta){
+    respuesta.sendFile(__dirname + '/recursos-estaticos/public/index.html');
+  });
+
+//SERVIDOR BUSCAR 
+
+app.get('/busqueda', function(consulta, respuesta) {
+    respuesta.sendFile(__dirname + '/recursos-estaticos/public/busqueda.html');
+});
 
 app.listen(puerto1, function() {
     console.log(`Servidor escuchando en ${puerto1}`)
-
 })
 
-//PARA AGREGAR IDEAS DE RECICLADO
-var upload_img = multer({ dest: "recursos-estaticos/secciones/images" })
-var upload_txt = multer({ dest: "recursos-estaticos/secciones/text" }) 
 
-app.post("/index", upload_img.single("foto_ejemplo"), upload_txt.single("newIdea"), function(consulta, respuesta){
-    db.collection.push({ 
+//PARA BUSCAR IDEAS DE RECICLADO
+var upload_img = multer({ dest: "recursos-estaticos/public/client-images" })
+/*var upload_txt = multer({ dest: "recursos-estaticos/secciones/text" })*/ 
+
+var db_ideas = [ ]
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get("/api/index", function(_ , respuesta) {
+    respuesta.json({ ideas: db_ideas })
+}
+)
+
+app.get("/api/index/:busqueda", function(consulta, respuesta) {
+    var busqueda = consulta.params.busqueda
+    var idea = db_ideas.find(function (i) {
+        return i.busqueda/*.toLowerCase() === busqueda.toLowerCase()*/
+    })
+
+    if (!idea) {
+        respuesta.status(404).json({ mensaje: "Mmm... Algo de esto pudo haber pasado: " +
+        "1º Hay un error en el tipeo de la búsqueda, " + 
+        "2º No existe reciclaje para lo insertado en la búsqueda, " +
+        "3º Aún no contamos con ideas de reciclaje de lo buscado" +
+        "4º Hay un error mío" })
+    }
+
+    respuesta.json(idea)
+})
+
+//PARA AGREGAR IDEAS
+app.post("/index", upload_img.single("foto_ejemplo"), /*upload_txt.single("newIdea"),*/ function(consulta, respuesta){
+    db_ideas.push({ 
         newIdea: consulta.body.newIdea,
         foto_ejemplo: "/images" + consulta.file.filename
     })
-        respuesta.status(201).redirect("/reciclaje-carton")
+        respuesta.status(201).redirect("/index")
 })
 
 //base de datos con info que ya tengo
